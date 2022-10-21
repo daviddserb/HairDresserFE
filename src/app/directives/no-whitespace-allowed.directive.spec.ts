91752014107eff00f17b57cc74b9374c2255471b
david <daviddserb@yahoo.com>
2022-10-21 12:59:29 +0300

    FinishInternship

--- src/app/directives/no-whitespace-allowed.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NoWhitespaceAllowedDirective } from './no-whitespace-allowed.directive';
-
-describe('NoWhitespaceAllowedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NoWhitespaceAllowedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
