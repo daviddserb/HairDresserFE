300bd56e0a739950be18b06c8a76c9fc8ad30465
david <daviddserb@yahoo.com>
2026-05-03 11:51:44 +0300

    Fix admin page get all employees, where workingDay.Name was empty because of the WorkingDay update from db table to enum, by creating an enum and displaying its correlating integer values to strings.

---
+++ src/app/models/WorkingDay.ts
@@ -0,0 +1,7 @@
+export enum WorkingDay {
+  Monday = 1,
+  Tuesday = 2,
+  Wednesday = 3,
+  Thursday = 4,
+  Friday = 5
+}
--- src/app/users-functionalities/employees/get-all-employees/get-all-employees.component.ts
+++ src/app/users-functionalities/employees/get-all-employees/get-all-employees.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HairDresserService } from 'src/app/services/hairdresser.service';
+import { WorkingDay } from 'src/app/models/WorkingDay';
 
 @Component({
   selector: 'app-get-all-employees',
@@ -8,14 +9,13 @@ import { HairDresserService } from 'src/app/services/hairdresser.service';
 })
 export class GetAllEmployeesComponent implements OnInit {
   displayedColumns: string[] = ['#', 'employeeName', 'employeeHairServices', 'employeeWorkingIntervals'];
-  
   allEmployees$: any;
+  WorkingDay = WorkingDay;
 
   constructor(private hairdresserService: HairDresserService) { }
 
   ngOnInit(): void {
     this.hairdresserService.getAllEmployees().subscribe(res => {
-      
       this.allEmployees$ = res;
     });
   }
